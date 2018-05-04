6f11e57bd173ca43806492e3a092dc6dfb788612
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-05-04 16:50:44 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        Hello Immimimmimmmmashcha!!!!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
