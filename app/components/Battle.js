83d2d3281837498c0aac133811dba3558af92f39
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-05-08 13:55:00 +0200

    more refactoring to remove constructor calls, and continued Battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,12 +1,144 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class Battle extends React.Component{
+const PlayerPreview = (props) => {
+  return (
+    <div>
+      <div className='column'>
+        <img
+          className='avatar'
+          src={props.avatar}
+          alt={props.username + 'avatar'}
+        />
+        <h2 className='username'>@{props.username}</h2>
+      </div>
+      <button className='reset'
+        onClick={() => props.onReset(props.id)}
+      >
+        reset
+      </button>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
+}
+class PlayerInput extends React.Component{
+  state = {username: ''};
+
+  handleChange = (event) => {
+    let val = event.target.value;
+    this.setState(() => ({username: val}))
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.props.id, this.state.username);
+  }
+
   render(){
     return (
-      <div>
-        Battle!
-      </div>
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input
+          id='username'
+          placeholder='github username'
+          autoComplete='off'
+          type='text'
+          className=''
+          value={this.state.username}
+          onChange={this.handleChange}>
+        </input>
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
+          Submit
+        </button>
+      </form>
     )
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+class Battle extends React.Component {
+
+  state = {
+      playerOneName: "",
+      playerTwoName: "",
+      playerOneImage: null,
+      playerTwoImage: null
+  };
+  
+
+  handleSubmit = (id, username) => {
+    this.setState(() => {
+      let newState = {};
+      newState[id + "Name"] = username;
+      newState[id + "Image"] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    });
+  }
+
+  handleReset = (id) => {
+    this.setState( () => {
+      return {
+        [id + 'Name']: "",
+        [id + 'Image']: null,
+      }
+    })
+  }
+
+  render() {
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
+    let playerOneImage = this.state.playerOneImage;
+    let playerTwoImage = this.state.playerTwoImage;
+
+    return <div className="row">
+        {!playerOneName && 
+          <PlayerInput
+            id='playerOne'
+            label='Player One'
+            onSubmit={this.handleSubmit} />
+        }
+        
+        {playerOneImage !== null &&
+          <PlayerPreview 
+            id='playerOne'
+            avatar={playerOneImage}
+            username={playerOneName}
+            onReset={() => this.handleReset('playerOne')}
+          />
+        }
+        
+        {!playerTwoName && 
+          <PlayerInput
+            id='playerTwo'
+            label='Player Two'
+            onSubmit={this.handleSubmit} />
+        }
+        
+        {playerTwoImage !== null &&
+          <PlayerPreview 
+            id='playerTwo'
+            avatar={playerTwoImage}
+            username={playerTwoName}
+            onReset={() => this.handleReset('playerTwo')}
+          />
+        }
+        
+      </div>;
   }
 }
 
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,107 +1,104 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import api from '../utils/api';
+import React from "react";
+import PropTypes from "prop-types";
+import api from "../utils/api";
 
-const SelectLanguage = (props) => {
-
-  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'Css', 'Python'];
+const SelectLanguage = props => {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "Css", "Python"];
 
   return (
-    <ul className='languages'>
+    <ul className="languages">
       {languages.map(v => {
-          return (
-            <li
-              className={v === props.selectedLanguage ? 'selected' : ''}
-              onClick={ () => props.onSelect(v) }
-              key={v}>
-              {v}
-            </li>
-          )
-        })
-      }
+        return (
+          <li
+            className={v === props.selectedLanguage ? "selected" : ""}
+            onClick={() => props.onSelect(v)}
+            key={v}
+          >
+            {v}
+          </li>
+        );
+      })}
     </ul>
   );
-}
+};
 
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
-}
+};
 
-const RepoGrid = (props) => {
+const RepoGrid = props => {
   return (
-    <ul className='popular-list'>
-      {
-        props.repos.map( (repo, idx) => {
-          return (
-            <li key={repo.url} className='popular-item'>
-              <div className='popular-rank'>{idx+1}</div>
-              <ul className='space-list-items'>
-                <li>
-                  <img
-                    className='avatar'
-                    src={repo.owner.avatar_url}
-                    alt={`Avatar for ${repo.owner.login}`}
-                  />
-                </li>
-                <li><a href={repo.html_url}>{repo.name}</a></li>
-                <li>@{repo.owner.login}</li>
-                <li>{repo.stargazers_count} stars</li>
-              </ul>
-            </li>
-          )    
-        })
-      }
+    <ul className="popular-list">
+      {props.repos.map((repo, idx) => {
+        return (
+          <li key={repo.url} className="popular-item">
+            <div className="popular-rank">{idx + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img
+                  className="avatar"
+                  src={repo.owner.avatar_url}
+                  alt={`Avatar for ${repo.owner.login}`}
+                />
+              </li>
+              <li>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        );
+      })}
     </ul>
   );
-}
+};
 
 RepoGrid.propTypes = {
   repos: PropTypes.array.isRequired
-}
+};
 
-class Popular extends React.Component{
+class Popular extends React.Component {
+  
+  state = {
+    selectedLanguage: "All",
+    repos: null
+  };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      selectedLanguage: 'All',
-      repos: null
-    };
-  }
-
-  componentDidMount(){
+  componentDidMount = () => {
     this.updateLanguage(this.state.selectedLanguage);
   }
 
-  async updateLanguage(lang) {
-    this.setState( () => {
-      return { 
+  updateLanguage = async (lang) => {
+    this.setState(() => {
+      return {
         selectedLanguage: lang,
         repos: null
       };
     });
 
     let repos = await api.fetchPopularRepos(lang);
-    this.setState( () => ({repos}));
+    this.setState(() => ({ repos }));
   }
 
-  render(){
+  render() {
     return (
       <div>
         <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
-          onSelect={(lang) => this.updateLanguage(lang)}
+          onSelect={this.updateLanguage}
         />
-        { !this.state.repos
-            ? <p>Loading...</p>
-            : <RepoGrid repos={this.state.repos} />
-        }
-        <pre><code className='debug'>
-          {this.state.repos
-            ? JSON.stringify(this.state.repos, null, 2)
-            : ''}
-        </code></pre>
+        {!this.state.repos ? (
+          <p>Loading...</p>
+        ) : (
+          <RepoGrid repos={this.state.repos} />
+        )}
+        <pre>
+          <code className="debug">
+            {this.state.repos ? JSON.stringify(this.state.repos, null, 2) : ""}
+          </code>
+        </pre>
       </div>
     );
   }
